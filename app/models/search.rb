89e08839de3dc69a81b0630e8fde5659f7b66495
Benedict Aluan <benedict.aluan@gmail.com>
2016-07-25 14:13:10 +1200

    Remove initialize override

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,13 +1,2 @@
 class Search < Supplejack::Search
-  
-  def initialize(params={})
-    params ||= {}
-    params[:i] ||= {}
-    params[:or] ||= {}
-    self.and ||= {}
-    self.or ||= {}
-
-    super(params)
-    self.or.merge!(params[:or])
-  end
 end
